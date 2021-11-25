8025069d9f88ffc0ee0f75af4969e8d07f2eed3f
student <amalendukm1999@gmail.com>
2021-11-25 18:30:12 +0530

    fourth commit

---
+++ sum.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main()
+{
+	int a,b,sum;
+	printf("\n Enter the first num:");
+	scanf("%d",&a);
+	printf("\n Enter the second num:");
+	scanf("%d",&b);
+	sum=a+b;
+	printf("\n sum=%d",sum);
+	return(0);
+}
+	
+
